058f276d9a21395961d0f85fd0d5d7538a762f27
MoebiusCh <phathentaidz2004@gmail.com>
2023-04-05 02:57:28 +0700

    update Cart

--- js/product.js
+++ js/product.js
@@ -14,16 +14,26 @@ const ObjNamingKey = {
     Quantity: "Quantity_",
     Cost: "Cost_"
 }
-/* Get Value every times reload page */
+var Cart = {
+    item: []
+};
+/* Get Value from local Storage every times reload page */
 window.addEventListener('load', () => {
     getValueFromLocalStorage();
 })
 function getValueFromLocalStorage() {
-    /* lấy value của key Storage */
-    const valueOfKeyStorage = window.localStorage.getItem(ObjNamingKey.Quantity + fileName);
+    /* lấy value of key Storage */
+    const valueOfKeyStorage = localStorage.getItem(ObjNamingKey.Quantity + fileName);
     if (valueOfKeyStorage != undefined) {
         totalQuantityProduct = parseFloat(valueOfKeyStorage);
     } console.log(totalQuantityProduct);
+
+    /* Get cart Item*/
+    const cartFromLocalStorage = JSON.parse(localStorage.getItem('Cart'));
+    for (let key in cartFromLocalStorage) {
+        Cart.item.push(cartFromLocalStorage[key]);
+    }
+
 }
 
 quantityProduct.addEventListener('change', () => {
@@ -48,31 +58,51 @@ function UpdateQuantityProduct(choose) {
         InputQuantityProduct += 1;
         quantityProduct.value = InputQuantityProduct;
         quantityProduct.setAttribute("value", InputQuantityProduct);
-    } else if (choose == -1) {
+    }
+    if (choose == -1) {
         if (InputQuantityProduct == 0) {
             return;
         }
         InputQuantityProduct -= 1;
         quantityProduct.value = InputQuantityProduct;
         quantityProduct.setAttribute("value", InputQuantityProduct);
-    } else if (choose = "Typing") {
+    } if (choose = "Typing") {
         if (quantityProduct.value < 0) {
             quantityProduct.value = 0;
             quantityProduct.setAttribute("value", 0);
-        } else {
-            quantityProduct.setAttribute("value", quantityProduct.value);
         }
+        quantityProduct.setAttribute("value", quantityProduct.value);
+        InputQuantityProduct = parseFloat(quantityProduct.value);
     }
 }
 
 function setLocalStorage() {
+    var ProductDetails = document.querySelector('.product-details');
+    var ProductTitle = ProductDetails.querySelector('.title').innerText;
+    var ImagePath = ProductDetails.querySelector('.media-image img').src;
 
     totalQuantityProduct += InputQuantityProduct;
 
-    console.log(totalQuantityProduct)
+    updateCartItem(ProductTitle, ImagePath);
 
-    window.localStorage.setItem(ObjNamingKey.Quantity + fileName, totalQuantityProduct);
-    window.localStorage.setItem(ObjNamingKey.Cost + fileName, cost.innerText);
+    console.log('Cart Item: ', Cart.item);
+    console.log('TotalQuantity: ', totalQuantityProduct);
 
+    localStorage.setItem(ObjNamingKey.Quantity + fileName, totalQuantityProduct);
+    localStorage.setItem(ObjNamingKey.Cost + fileName, cost.innerText);
+    localStorage.setItem('Cart', JSON.stringify(Cart.item))
 }
 
+function updateCartItem(ProductTitle, ImagePath) {
+    var flag = true;
+
+    for (let key in Cart.item) {
+        if (Cart.item[key].name == ProductTitle) {
+            Cart.item[key].quantity = totalQuantityProduct;
+            flag = false;
+        }
+    }
+    if (flag) {
+        Cart.item.push({ name: ProductTitle, imgPath: ImagePath, quantity: totalQuantityProduct });
+    }
+}
--- js/store.js
+++ js/store.js
@@ -35,29 +35,8 @@ function ready() {
         }
     ];
     /*  Render Element Shopping Cart*/
-    let productNeedToRenderList = [];
-    for (let i in productList) {
-        productNeedToRenderList.push(`
-            <img src="${productList[i].pathImage}"
-                class="shop-item-image">
-    
-            <div class="shop-item-details">
-                <h3 class="item-title">${productList[i].name}</h3>
-            </div>
-            <span class="price"></span>
-            <span class="quantity-container">
-                <button name="decrement">-</button> 
-           <input style="width:30px" value="1" name="quantity"></input>
-                <button name="increment">+</button>
-           </span>
-           <span class="delete"><img src="${productList[i].deleteBtn}" alt="" class="remove-icon"></span>`
-        )
-        let shop_item = document.createElement("div");
-        shop_item.classList.add("shop-item");
-        shop_item.innerHTML = productNeedToRenderList[i];
-        const PHX = contentContainer.appendChild(shop_item);
-        // init other productList
-    }
+    RenderElement(productList, contentContainer);
+
     /* Update table data */
     let quantity = document.querySelectorAll('input[name="quantity"]');
     console.log(quantity.value)
@@ -96,4 +75,28 @@ function ready() {
 }
 
 
-
+function RenderElement(productList, contentContainer) {
+    let productNeedToRenderList = [];
+    for (let i in productList) {
+        productNeedToRenderList.push(`
+            <img src="${productList[i].pathImage}"
+                class="shop-item-image">
+    
+            <div class="shop-item-details">
+                <h3 class="item-title">${productList[i].name}</h3>
+            </div>
+            <span class="price"></span>
+            <span class="quantity-container">
+                <button name="decrement">-</button> 
+           <input style="width:30px" value="1" name="quantity"></input>
+                <button name="increment">+</button>
+           </span>
+           <span class="delete"><img src="${productList[i].deleteBtn}" alt="" class="remove-icon"></span>`
+        )
+        let shop_item = document.createElement("div");
+        shop_item.classList.add("shop-item");
+        shop_item.innerHTML = productNeedToRenderList[i];
+        const PHX = contentContainer.appendChild(shop_item);
+        // init other productList
+    }
+}
